9d5b2e2fb5ea79dbfde96ffc1cf3d6d3ce79c53d
mark wijkhuizen <markwijkhuizen@hotmail.com>
2016-06-08 21:01:22 +0200

    Help activity scroll fix
    Help activity scroll gefixt en typo verbeterd.

--- app/src/main/java/com/example/puC/super42/Help.java
+++ app/src/main/java/com/example/puC/super42/Help.java
@@ -24,10 +24,10 @@ public class Help extends Activity {
                 " - Als een bolletje groter wordt dan 42 is het gameover. " + "<br/>" + "<br/>" +
                 " - Door een bolletje aan te raken en te slepen kan je een route maken die het bolletje zal volgen. " + "<br/>" + "<br/>" +
                 " - Deze paden kunnen niet meer aangepast worden, dus wees gewaarschuwd!"  + "<br/>" + "<br/>" +
-                " - Nieuwe bolletjes spawnen langs de rand van het scherm. " + "<br/>" + "<br/>" +
+                " - Nieuwe bolletjes spawnen een afstandje van de andere bolletjes maar de richting is willekeurig. " + "<br/>" + "<br/>" +
                 " - Boven in het game-scherm staan een timer en een powe.r" + "<br/>" + "<br/>" +
                 " - Als de timer afloopt word er een power geactiveerd. " + "<br/>" + "<br/>" +
-                " - Deze power is positief als je een 42 gemaakt hebt voor de timer aflooptbanders is de power negatief." +  "<br/>" + "<br/>" +
+                " - Deze power is positief als je een 42 gemaakt hebt voor de timer afloopt anders is de power negatief." +  "<br/>" + "<br/>" +
                 " - Het effect van de power staat ook bovenaan het scherm."
 
         ));
